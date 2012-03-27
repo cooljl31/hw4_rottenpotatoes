2db2602b25436a06ad812aed11a30f618efbe9ea
Kyle Lieber <klieberuni@gmail.com>
2012-03-26 19:49:10 -0700

    working on it

---
+++ spec/director_spec.rb
@@ -0,0 +1,7 @@
+
+describe MoviesController, "displaying director field" do
+  #it "returns true for 'A man, a plan, a canal -- Panama'" do
+  #  palindrome?("A man, a plan, a canal -- Panama").should eq(true)
+  #end
+end
+
